8b9457d992bc8e9ce0979c442a8cf612530bc6e5
tuana <tuananh45468@gmail.com>
2023-04-06 15:12:58 +0700

    finish project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+const color = require('tailwindcss/colors')
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    colors: {
+      "white": "hsl(0, 0%, 100%)",
+      "light-gray": "hsl(212, 45%, 89%)",
+      "grayish-blue": "hsl(220, 15%, 55%)",
+      "dark-blue": "hsl(218, 44%, 22%)",
+          ...color
+    },
+
+
+    extend: {
+      fontFamily: {
+        'outfit': ['Outfit', 'sans-serif'],
+      },
+
+      fontWeight:{
+        'normal': 400,
+        'bold': 700
+      },
+    },
+    plugins: [],
+  }
 }
 
